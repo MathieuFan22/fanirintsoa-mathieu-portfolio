c4f3413d4129e232525e66e6f27b14f835c41d43
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-01-27 22:47:49 +0300

    Add hugeicons-react package as a dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { ArrowRight02Icon, Home01Icon } from "hugeicons-react";
 
 function App() {
 
@@ -7,11 +8,24 @@ function App() {
     <div className="container">
       <div className="card">
         <div className="box">
-
           <div className="icon">
-            <div className="iconBox"></div>
+            <div className="iconBox">
+              <div className="lastname">Fanirintsoa</div>
+              <div className="firstname">Mathieu</div>
+            </div>
           </div>
-          <div className="content"></div>
+          <div className="content">
+              <div className="circle">
+
+              </div>
+              <div className="presentation">
+                <div className="light-text">I'm</div>
+                <div className="light-text-name">Fanirinstoa</div>
+                <div className="bold-text-name">Mathieu</div>
+                <div className="get-in-touch"><ArrowRight02Icon size={32} color="#000"/></div>
+                
+              </div>
+          </div>
           
         </div>
       </div>
