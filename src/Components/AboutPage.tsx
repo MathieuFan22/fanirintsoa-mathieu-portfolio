f87b36fd7afec954d449fd6829d78e9fc3529362
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-02-04 22:48:45 +0300

    UI: About section

--- src/Components/AboutPage.tsx
+++ src/Components/AboutPage.tsx
@@ -16,22 +16,37 @@ const AboutPage: React.FC = () => {
         </Fade>
         <div className="line"></div>
       </div>
-      <Fade direction="right" >
-      <div className="section-content">
-        <div className="left-side">
-          <div className="image-wrapper">
-            <img className="image-circle" src="/me.png" alt="Profile" />
+      <Fade direction="right">
+        <div className="section-content">
+          <div className="left-side">
+            <p className="intro">
+              <span className="alinea">I</span> am Fanirintsoa Mathieu, a
+              Malagasy developer and graphic designer based in Antananarivo.
+              Currently pursuing higher education at ISPM Antsobolo in Computer
+              Science, Applied Statistics, and Artificial Intelligence. Besides
+              my studies, I also work as a frontend developer and graphic
+              designer freelancer.
+            </p>
+            <p className="intro">
+              I was passionate about the IT world since my childhood, and I have
+              a deep appreciation for visual arts, including graphic design,
+              illustration, drawing, photography, and videography.
+            </p>
+          </div>
+          <div className="right-side">
+            <div className="image-wrapper">
+              <img className="image-circle" src="/me.png" alt="Profile" />
+            </div>
+            <div className="whatido">
+              <div className="whatido-item dev">Mobile App</div>
+              <div className="whatido-item dev">Desktop App</div>
+              <div className="whatido-item dev">Website</div>
+              <div className="whatido-item design">Social media Designs</div>
+              <div className="whatido-item design">UI/UX Designs</div>
+              <div className="whatido-item design">Logo Designs</div>
+            </div>
           </div>
         </div>
-        <div className="right-side">
-          <p>I'm Mathieu, a Malagasy developer and graphic designer with a strong interest in the digital world. 
-            I studied at ISPM and am deeply involved in visual art. Curious and self-taught, 
-            I enjoy creating innovative and visually appealing solutions that blend performance with creativity. 
-            As a freelancer, I’m always eager to take on new projects and adapt to new challenges. 
-            My goal is to turn ideas into unique and impactful digital experiences. 
-            Feel free to reach out if you're looking for someone to bring your vision to life!</p>
-        </div>
-      </div>
       </Fade>
     </div>
   );
