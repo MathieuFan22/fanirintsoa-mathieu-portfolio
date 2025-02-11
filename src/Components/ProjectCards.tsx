04f8ad11dd90cab0725b804983ba5cc08e8cc7cd
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-02-11 23:27:48 +0300

    Add : Project Page Card

--- src/Components/ProjectCards.tsx
+++ src/Components/ProjectCards.tsx
@@ -1,20 +1,19 @@
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ProjectCards() {
   return (
-    <div className='card-container'>
+    <div className="card-container">
       <div className="card-image">
-        <img src="/me.png" alt="Profile" />
+        <img src="/Projects/test.jpg" alt="Profile" />
       </div>
       <div className="card-title">
         <div className="project-title">
-          <h1>Project Title</h1>
+          <h3>Project Title</h3>
         </div>
         <div className="line"></div>
         <div className="github-link">
-        <a
+          <a
             href="https://github.com/MathieuFan22"
             target="_blank"
             rel="noopener noreferrer"
@@ -22,19 +21,20 @@ function ProjectCards() {
             <FontAwesomeIcon icon={faGithub} />
           </a>
         </div>
-        <div className="project-tech">
-          <p>React Ts - Nest</p>
-        </div>
-        <div className="project-description">
-          <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Pariatur reprehenderit odio esse sapiente obcaecati autem suscipit a excepturi ea quia, nisi nihil, 
-              sed accusamus totam eius perspiciatis officiis porro saepe.
-          </p>
-        </div>
+      </div>
+      <div className="project-tech">
+        <p>React Ts - Nest</p>
+      </div>
+      <div className="project-description">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+          reprehenderit odio esse sapiente obcaecati autem suscipit a excepturi
+          ea quia, nisi nihil, sed accusamus totam eius perspiciatis officiis
+          porro saepe.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProjectCards
+export default ProjectCards;
