51ef60257b79ddf82941f55a19fbd00f7aad36d5
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-02-22 01:12:03 +0300

    UI: Infinite Slider

--- src/Components/InfiniteSlider.tsx
+++ src/Components/InfiniteSlider.tsx
@@ -2,7 +2,21 @@ import React from "react";
 import "./InfiniteSlider.css";
 
 const technologies = [
-  "Flutter", "React TS", "Node.js", "JavaScript", "TypeScript", "CSS", "HTML", "MongoDB", "Figma", "A. Illustrator", "Nest Js", "Git", "Github"
+  "Flutter",
+  "React TS",
+  "Node.js",
+  "JavaScript",
+  "TypeScript",
+  "CSS",
+  "HTML",
+  "MongoDB",
+  "Figma",
+  "A. Illustrator",
+  "A. Photoshop",
+  "A. Xd",
+  "Nest Js",
+  "Git",
+  "Github",
 ];
 
 const InfiniteSlider: React.FC = () => {
@@ -10,7 +24,9 @@ const InfiniteSlider: React.FC = () => {
     <div className="slider-container">
       <div className="slider">
         {[...technologies, ...technologies].map((tech, index) => (
-          <div key={index} className="text-item">{tech} <div className="dot-spacing"></div></div>
+          <div key={index} className="text-item">
+            {tech} <div className="dot-spacing"></div>
+          </div>
         ))}
       </div>
     </div>
