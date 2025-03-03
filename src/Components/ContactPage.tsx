d9738afbe19a4cb9eab9629342be0f58ae654076
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-03-03 21:48:49 +0300

    Update : Contact section

--- src/Components/ContactPage.tsx
+++ src/Components/ContactPage.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./contact-page.css";
 import { Fade } from "react-awesome-reveal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faWhatsapp,
+  faXTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const ContactPage: React.FC = () => {
-  
   return (
-    <div className="project-section">
+    <div className="contact-section">
       <div className="section-title">
         <Fade direction="left">
           <div className="dot"></div>
@@ -14,7 +21,54 @@ const ContactPage: React.FC = () => {
         <div className="line"></div>
       </div>
       <div className="section-content">
-        
+        <h2 className="get-in-touch">GET IN TOOUCH</h2>
+        <p className="contact-info">
+          Got a project idea, or need a developer or graphic designer? Don’t hesitate to reach
+          out! I'm always open to new opportunities, collaborations, or a
+          friendly chat. Let’s bring your ideas to life, I'll get back to you as soon as possible!
+        </p>
+        <div className="contact-social-links">
+          <a
+            href="https://www.linkedin.com/in/mathieu22"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+            Fanirintsoa Mathieu
+          </a>
+          <a
+            href="https://wa.me/qr/K6TKT2TLZTAWN1"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faWhatsapp} />
+            +261 34 72 363 79
+          </a>
+          <a
+            href="https://www.facebook.com/fanirintsoa.mathieu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+            Fanirintsoa Mathieu
+          </a>
+          <a
+            href="https://x.com/Fanirintsoa_22"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faXTwitter} />
+            Fanirintsoa Mathieu
+          </a>
+          <a
+            href="https://github.com/MathieuFan22"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+            MathieuFan22
+          </a>
+        </div>
       </div>
     </div>
   );
