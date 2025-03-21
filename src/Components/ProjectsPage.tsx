a14e7882f6b4e3c46dc3f27cb4d0bc681045153f
MathieuFan22 <fanirintsoaandrianandraina@gmail.com>
2025-03-21 22:48:34 +0300

    Project Card Update

--- src/Components/ProjectsPage.tsx
+++ src/Components/ProjectsPage.tsx
@@ -38,11 +38,25 @@ const ProjectsPage: React.FC = () => {
 };
 const project = [
   {
-    imageSrcUrl: "BoardGames.jpg",
+    imageSrcUrl: "BoardGames.png",
     projectTitle: "Board Games",
     projectTech: "React - Typescript - CSS",
     projectDescription:
-      "A digital board game platform that offers a variety of games, allowing players to compete against each other or challenge an AI in VS computer mode.",
+      "A digital board game web platform that offers a variety of games, allowing players to compete against each other or challenge an AI in VS computer mode.",
+  },
+  {
+    imageSrcUrl: "RNA.png",
+    projectTitle: "Prédiction de séries temporelles",
+    projectTech: "React - Typescript - Tauri - CSS",
+    projectDescription:
+      "A desktop application for time series prediction that utilizes multilayer artificial neural networks to analyze and forecast the Hénon series. It provides interactive visualizations and supports both one-step-ahead and multi-step-ahead (3-10-20 steps) predictions.",
+  },
+  {
+    imageSrcUrl: "Portfolio.png",
+    projectTitle: "My Portfolio",
+    projectTech: "React TS- Framer Motion - Lenis - CSS",
+    projectDescription:
+      "A personal portfolio website with modern and interactive UI designed to showcase my skills, projects, and achievements in software development, AI, and design.",
   },
   {
     imageSrcUrl: "Moradis.jpg",
@@ -57,13 +71,6 @@ const project = [
     projectTech: "Adobe Illustrator - Figma",
     projectDescription:
     "A logo for a mobile app for communication between hearing and deaf or mute individuals, it features a hand gesture that represents the letter “S” in alphabetic sign language.",
-  },
-  {
-    imageSrcUrl: "BoardGames.jpg",
-    projectTitle: "Board Games",
-    projectTech: "React - Typescript - CSS",
-    projectDescription:
-    "A digital board game platform that offers a variety of games, allowing players to compete against each other or challenge an AI in VS computer mode.",
   },
 ];
 export default ProjectsPage;
